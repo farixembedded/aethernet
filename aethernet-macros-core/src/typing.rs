a3b066b66487210025293a5c1e279c2c1860e5d9
Matt Campbell <matt@farixembedded.com>
2025-07-05 00:44:55 -0400

    typing feels complete with unit tests

--- aethernet-macros-core/src/typing.rs
+++ aethernet-macros-core/src/typing.rs
@@ -1,12 +1,12 @@
 // Copyright 2025 Farix Embedded LLC
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-//! Helper for parsing and validating argument type info for the ICD
+//! Helper for parsing and validating argument type info from the ICD
 
 use quote::ToTokens;
 use syn::spanned::Spanned;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum AethernetType {
     // integers
     U8,
@@ -29,7 +29,7 @@ pub enum AethernetType {
         e: Box<AethernetType>,
     },
     Option(Box<AethernetType>),
-    // Compound types
+    // Compound/container types
     Vec(Box<AethernetType>),
     Array {
         t: Box<AethernetType>,
@@ -45,7 +45,7 @@ pub fn parse_type(ty: &syn::Type) -> Result<AethernetType, syn::Error> {
     use syn::Type;
     match ty {
         Type::Array(array) => parse_array(array),
-        Type::Tuple(_) => Ok(AethernetType::Todo),
+        Type::Tuple(tuple) => parse_tuple(tuple),
         Type::Path(path) => parse_path(path),
         // all others invalid
         _ => Err(syn::Error::new(ty.span(), "Invalid Aethernet Type")),
@@ -75,6 +75,16 @@ fn parse_array(array: &syn::TypeArray) -> Result<AethernetType, syn::Error> {
     })
 }
 
+fn parse_tuple(tuple: &syn::TypeTuple) -> Result<AethernetType, syn::Error> {
+    let mut inners = vec![];
+
+    for ty in &tuple.elems {
+        inners.push(parse_type(ty)?);
+    }
+
+    Ok(AethernetType::Tuple(inners))
+}
+
 fn parse_path(path: &syn::TypePath) -> Result<AethernetType, syn::Error> {
     if path.qself.is_some() {
         return Err(syn::Error::new(
@@ -92,7 +102,7 @@ fn parse_path(path: &syn::TypePath) -> Result<AethernetType, syn::Error> {
     }
 
     let path = path.segments.first().unwrap(); // unwrap safe, we checked above
-    // extract basic types
+    // extract basic types (confirming there are no path args)
     if path.arguments == syn::PathArguments::None {
         match path.ident.to_string().as_str() {
             "u8" => return Ok(AethernetType::U8),
@@ -111,13 +121,231 @@ fn parse_path(path: &syn::TypePath) -> Result<AethernetType, syn::Error> {
         }
     }
 
-    // todo types:
-    // Result
-    // Option
-    // Vec
-    if path.ident.to_string().as_str() == "Vec" {
-        eprintln!("Got a vec! {path:?}");
+    match path.ident.to_string().as_str() {
+        "Vec" => {
+            let [inner_type] = parse_bracket_inner::<1>(path)?;
+            Ok(AethernetType::Vec(inner_type.into()))
+        }
+        "Option" => {
+            let [inner_type] = parse_bracket_inner::<1>(path)?;
+            Ok(AethernetType::Option(inner_type.into()))
+        }
+        "Result" => {
+            let [t, e] = parse_bracket_inner::<2>(path)?;
+            Ok(AethernetType::Result { t: t.into(), e: e.into() })
+        }
+        _ => Err(syn::Error::new(path.span(), "Invalid Aethernet type")),
+    }
+}
+
+/// Parse an arbitrary number of inner types from brackets like `Vec<Ty1, Ty2, Ty3, ..>`
+/// returns an error if the number of args doesn't match the requested
+fn parse_bracket_inner<const N: usize>(
+    path: &syn::PathSegment,
+) -> Result<[AethernetType; N], syn::Error> {
+    match &path.arguments {
+        syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+            args, ..
+        }) => {
+            // rework args into a vec so we can do all vec operations on it
+            let mut aethernet_args = vec![];
+            for arg in args {
+                match arg {
+                    syn::GenericArgument::Type(ty) => aethernet_args.push(parse_type(ty)?),
+                    _ => return Err(syn::Error::new(path.span(), "Incorrect arguments")),
+                }
+            }
+
+            aethernet_args
+                .try_into()
+                .map_err(|_| syn::Error::new(path.span(), "Incorrect number of arguments"))
+        }
+        _ => Err(syn::Error::new(path.span(), "Incorrect arguments")),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test_case::test_case;
+
+    // helper to parse an input string with syn, and convert it to an AethernetType (or panic on
+    // failure)
+    fn input_to_aethernet_type(input: &str) -> AethernetType {
+        parse_type(&syn::parse_str(input).unwrap())
+            .unwrap_or_else(|err| panic!("{input}: Couldn't parse: {err}"))
     }
 
-    Ok(AethernetType::Todo)
+    #[test_case("u8" => AethernetType::U8)]
+    #[test_case("i8" => AethernetType::I8)]
+    #[test_case("u16" => AethernetType::U16)]
+    #[test_case("i16" => AethernetType::I16)]
+    #[test_case("u32" => AethernetType::U32)]
+    #[test_case("i32" => AethernetType::I32)]
+    #[test_case("u64" => AethernetType::U64)]
+    #[test_case("i64" => AethernetType::I64)]
+    #[test_case("f32" => AethernetType::F32)]
+    #[test_case("f64" => AethernetType::F64)]
+    #[test_case("bool" => AethernetType::Bool)]
+    #[test_case("String" => AethernetType::String)]
+    fn test_primitive_type_parsing(input: &str) -> AethernetType {
+        input_to_aethernet_type(input)
+    }
+
+    #[test_case("[u8; -4]" ; "negative number")]
+    #[test_case("[u8; 4+4]" ; "expressions")]
+    #[test_case("[u8; 234adsf]" ; "malformed number")]
+    #[test_case("[u8; 2usize]" ; "literal type annotation")]
+    fn test_parse_array_size_failing_cases(input: &str) {
+        let result = parse_type(&syn::parse_str(input).unwrap());
+        assert!(result.is_err());
+    }
+
+    #[test_case("[u8; 4]" => AethernetType::Array { t: AethernetType::U8.into(), n: 4 }
+        ; "lenth simple number")]
+    #[test_case("[u8; 23]" => AethernetType::Array { t: AethernetType::U8.into(), n: 23 }
+        ; "simple number 2")]
+    #[test_case("[u8; 0]" => AethernetType::Array { t: AethernetType::U8.into(), n: 0 }
+        ; "zero")]
+    #[test_case("[u8; 153232963]" => AethernetType::Array { t: AethernetType::U8.into(), n: 153232963 }
+        ; "fairly large number")]
+    #[test_case("[u8; 1]" => AethernetType::Array { t: AethernetType::U8.into(), n: 1 }
+        ; "type u8")]
+    #[test_case("[u64; 1]" => AethernetType::Array { t: AethernetType::U64.into(), n: 1 }
+        ; "type u64")]
+    #[test_case("[String; 1]" => AethernetType::Array { t: AethernetType::String.into(), n: 1 }
+        ; "type String")]
+    #[test_case("[[f32; 4]; 8]" =>
+        AethernetType::Array {
+            t: AethernetType::Array {
+                t: AethernetType::F32.into(),
+                n: 4
+            }.into(),
+            n: 8
+        }
+        ; "nested")]
+    #[test_case("[Vec<f32>; 8]" =>
+        AethernetType::Array {
+            t: AethernetType::Vec(
+                AethernetType::F32.into()
+            ).into(),
+            n: 8
+        }
+        ; "nested compound")]
+    fn test_parse_array_passing(input: &str) -> AethernetType {
+        input_to_aethernet_type(input)
+    }
+
+    #[test_case("(i32,)" =>
+        AethernetType::Tuple(
+            vec![
+                AethernetType::I32,
+            ]
+        ) ; "single member tuple")]
+    #[test_case("(i32, u8, String, bool)" =>
+        AethernetType::Tuple(
+            vec![
+                AethernetType::I32,
+                AethernetType::U8,
+                AethernetType::String,
+                AethernetType::Bool,
+            ]
+        ) ; "multi member tuple")]
+    #[test_case("(i32, u8, String, (u16, i16))" =>
+        AethernetType::Tuple(
+            vec![
+                AethernetType::I32,
+                AethernetType::U8,
+                AethernetType::String,
+                AethernetType::Tuple(
+                    vec![
+                        AethernetType::U16,
+                        AethernetType::I16,
+                    ]
+                ),
+            ]
+        ) ; "nested tuples")]
+    #[test_case("(i32, Vec<String>)" =>
+        AethernetType::Tuple(
+            vec![
+                AethernetType::I32,
+                AethernetType::Vec(
+                    AethernetType::String.into()
+                )
+            ]
+        ) ; "compound nested tuples")]
+    fn test_parse_tuple_passing(input: &str) -> AethernetType {
+        input_to_aethernet_type(input)
+    }
+
+    #[test_case("Vec<i32>" => AethernetType::Vec(Box::new(AethernetType::I32)))]
+    #[test_case("Vec<bool>" => AethernetType::Vec(Box::new(AethernetType::Bool)))]
+    #[test_case("Vec<Vec<i32>>" =>
+        AethernetType::Vec(
+            AethernetType::Vec(
+                AethernetType::I32.into()
+            ).into()
+        ); "compound Vec")]
+    #[test_case("Vec<Vec<Vec<i32>>>" =>
+        AethernetType::Vec(
+            AethernetType::Vec(
+                AethernetType::Vec(
+                    AethernetType::I32.into()
+                ).into()
+            ).into()
+        ); "double compound Vec")]
+    #[test_case("Vec<[u8; 4]>" =>
+        AethernetType::Vec(
+            AethernetType::Array{
+                t:AethernetType::U8.into(),
+                n: 4,
+            }.into()
+        ); "compound nested type")]
+    fn test_parse_vec_passing(input: &str) -> AethernetType {
+        input_to_aethernet_type(input)
+    }
+
+    #[test_case("Option<u8>" => AethernetType::Option(AethernetType::U8.into()) ; "normal u8")]
+    #[test_case("Option<String>" => AethernetType::Option(AethernetType::String.into()) ; "normal String")]
+    #[test_case("Option<Option<f32>>" =>
+        AethernetType::Option(
+            AethernetType::Option(
+                AethernetType::F32.into()
+            ).into()
+        )
+        ; "nested")]
+    #[test_case("Option<Vec<f32>>" =>
+        AethernetType::Option(
+            AethernetType::Vec(
+                AethernetType::F32.into()
+            ).into()
+        )
+        ; "nested compound")]
+    fn test_option(input: &str) -> AethernetType {
+        input_to_aethernet_type(input)
+    }
+
+    #[test_case("Result<u8, String>" =>
+        AethernetType::Result{
+            t: AethernetType::U8.into(),
+            e: AethernetType::String.into(),
+        })]
+    #[test_case("Result<Result<u8, String>, bool>" =>
+        AethernetType::Result{
+            t: AethernetType::Result{
+                t: AethernetType::U8.into(),
+                e: AethernetType::String.into(),
+            }.into(),
+            e: AethernetType::Bool.into(),
+        }; "nested")]
+    #[test_case("Result<Vec<f32>, bool>" =>
+        AethernetType::Result{
+            t: AethernetType::Vec(
+                AethernetType::F32.into(),
+            ).into(),
+            e: AethernetType::Bool.into(),
+        }; "nested compound")]
+    fn test_result(input: &str) -> AethernetType {
+        input_to_aethernet_type(input)
+    }
 }
